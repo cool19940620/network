788754508263d6f7d4b7a365ab0e7f071da44068
cool19940620 <cool19940620@gmail.com>
2017-07-04 15:45:53 +0000

    add

--- UDP/listener/listener.c
+++ UDP/listener/listener.c
@@ -16,16 +16,10 @@
 #define MYPORT "4950"    // the port users will be connecting to
 
 #define MAXBUFLEN 100
-
+void sendsomething_udp(char *,char *,int);
+void *get_in_addr(struct sockaddr *);
 // get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
 
 int main(void)
 {
@@ -35,7 +29,8 @@ int main(void)
     int numbytes;
     struct sockaddr_storage their_addr;
     char buf[MAXBUFLEN];
-    socklen_t addr_len;
+	char *sendbuf;
+    socklen_t addr_len; //socklen_t 長度和int相同
     char s[INET6_ADDRSTRLEN];
 
     memset(&hints, 0, sizeof hints);
@@ -69,14 +64,19 @@ int main(void)
         fprintf(stderr, "listener: failed to bind socket\n");
         return 2;
     }
-
+    /*if ((numbytes = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&their_addr, &addr_len)) == -1) 
+	{
+        perror("talker: sendto");
+        exit(1);
+    }
+	*/
     freeaddrinfo(servinfo);  //使用完釋放
 
     printf("listener: waiting to recvfrom...\n");
 
     addr_len = sizeof their_addr;
-    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len)) == -1) 
+	{
         perror("recvfrom");
         exit(1);
     }
@@ -85,8 +85,62 @@ int main(void)
     printf("listener: packet is %d bytes long\n", numbytes);
     buf[numbytes] = '\0';
     printf("listener: packet contains \"%s\"\n", buf);
+	close(sockfd);
+	sendbuf="test123!!";
 
-    close(sockfd);
+	//printf("%s\n",sendbuf);
+	sendsomething_udp(s,sendbuf,strlen(sendbuf));
+
 
     return 0;
+}
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+void sendsomething_udp(char *ip,char *buf,int lenbuf)
+{
+	int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_len; //socklen_t 長度和int相同
+	
+	memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(ip, MYPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) 
+	{
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+    }
+
+    if ((numbytes = sendto(sockfd, buf, lenbuf, 0,
+             p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+    close(sockfd);
 }
